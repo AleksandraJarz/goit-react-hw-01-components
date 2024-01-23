ba9d1e3809bc1e28eff21d6bb0526d8f9cc0bdfd
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2024-01-23 17:25:04 +0100

    commit

---
+++ src/components/friendList/friendList.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './friendListItem/friendListItem.js';
+import css from './friendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          id={friend.id}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
+
+export default FriendList;
